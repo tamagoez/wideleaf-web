1a9b490f26d02b2884973452ec655c8d7b2c3c1b
tamagoez <87574268+tamagoez@users.noreply.github.com>
2023-08-29 16:05:25 +0000

    seems canvas has some error

--- next.config.js
+++ next.config.js
@@ -10,6 +10,7 @@ const nextConfig = {
     config.externals = [...config.externals, { canvas: "canvas" }];  // required to make Konva & react-konva work
     return config;
   },
+  
 };
 
 module.exports = nextConfig;
